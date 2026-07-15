594670cf1f6054710bb2de726355d740e0577a02
deepak chouhan <chouhandeep176@gmail.com>
2026-07-15 08:05:13 +0530

    logs added

--- backend/app.js
+++ backend/app.js
@@ -18,9 +18,16 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+  console.log("Incoming:", req.method, req.url);
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("api working");
 });
+
+
 
 // apply auth and user router -->
 app.use("/auth", authRouter);
