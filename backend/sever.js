b324d489f8dbe2964d2d28374ed739ced807e9a1
deepak chouhan <chouhandeep176@gmail.com>
2026-07-15 06:51:55 +0530

    error fixed

--- backend/sever.js
+++ backend/sever.js
@@ -9,7 +9,7 @@ dotenv.config();
 import connect_db from "./config/db.js";
 connect_db();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`app is running at http://localhost:${PORT}`);
